fd05847208ad81eff688a7c80f56a2a68ab3e351
SreenathT13 <90893979+SreenathT13@users.noreply.github.com>
2021-09-28 22:32:29 +0530

    Delete RX130_config.py

--- RX130_config.py
+++
@@ -1,6 +0,0 @@
-RtoL = '//*[@id="stepformcontainer"]/tx-elements[1]/div[2]/div/div/div/button'
-Live_video = '//*[@id="206ef6f3-5941-32e5-e741-0b3c4e4763e0"]/div/tx-elements/div[2]/div/div/div[1]/div/div[1]/em'
-S_btn = '//*[@id="stepformcontainer"]/tx-elements[4]/div[2]/div/div/div/button'
-G_btn ='//*[@id="52565085-0844-beff-7418-545c70ab1c19"]/div/tx-elements/div[2]/div/div[1]/div[2]/em'
-LtoR = '//*[@id="stepformcontainer"]/tx-elements[2]/div[2]/div/div/div/button'
-A_btn = '//*[@id="stepformcontainer"]/tx-elements[3]/div[2]/div/div/div/button'
